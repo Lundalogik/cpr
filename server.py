913af7af84d52cdd2929d913cc6e8052b4fbc29e
Filip Persson <filip.persson@me.com>
2014-01-24 08:18:38 +0100

    First commit!

---
+++ server.py
@@ -0,0 +1,19 @@
+import bottle
+from bottle import response
+from bottle import redirect
+from bottle import static_file
+import os
+from articleHandler import ArticleLoader
+from articleHandler import Article
+
+articles = ArticleLoader().load_articles_from_files("articles/")
+print(articles[0])
+
+cpr = bottle.app()
+
+@cpr.route('/')
+def send_static():
+    return static_file('/index.html', root='./')
+
+
+cpr.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
