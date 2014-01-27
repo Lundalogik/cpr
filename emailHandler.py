8112fb078934660adb1eb886dcbafc85ce7592c2
Filip Persson <filip.persson@me.com>
2014-01-27 19:01:47 +0100

    Updated

--- emailHandler.py
+++ emailHandler.py
@@ -4,28 +4,31 @@ from email.mime.text import MIMEText
 
 class SendEmail:
 
-	def __init__(self, email_address, articles):
-		self.email_address = email_address
+	def __init__(self,email_server_address, email_server_user, email_server_password, to_email_address, from_email_address, articles):
+		self.email_server = email_server_address
+		self.email_server_user = email_server_user
+		self.email_server_password = email_server_password
+		self.to_email_address = to_email_address
+		self.from_email_address = from_email_address
 		self.selected_articles = articles
 		self.send_mail()
 
 	def send_mail(self):
 		msg = MIMEMultipart()
-		msg['From'] = 'filip.persson@.se'
-		msg['To'] = 'filip.persson@.com'
-		msg['Subject'] = 'simple email in python'
+		msg['From'] = self.from_email_address
+		msg['To'] = self.to_email_address
+		msg['Subject'] = 'Hello from Python!'
 		message = 'here is the email'
 		msg.attach(MIMEText(message))
-		mailserver = smtplib.SMTP('', 587)
-		print("hepp!")
-		# identify ourselves to smtp gmail client
+		mailserver = smtplib.SMTP(self.email_server, 587)
+		# identify ourselves to smtp client
 		mailserver.ehlo()
 		# secure our email with tls encryption
 		mailserver.starttls()
 		# re-identify ourselves as an encrypted connection
 		mailserver.ehlo()
-		mailserver.login('', '')
+		mailserver.login(self.email_server_user, self.email_server_password)
 
-		mailserver.sendmail('filip.persson@.se','filip.persson@.com',msg.as_string())
+		mailserver.sendmail(self.to_email_address,self.from_email_address,msg.as_string())
 
 		mailserver.quit()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import bottle
 from bottle import response
 from bottle import redirect
 from bottle import static_file
+from bottle import request
 
 import os
 import json
@@ -9,20 +10,30 @@ import json
 from articleHandler import ArticleLoader
 from emailHandler import SendEmail
 
-
-articles = ArticleLoader().load_articles_from_files("articles/")
-SendEmail("filip.persson@.com", articles)
-
+articles = {}
+articles['articles'] = ArticleLoader().load_articles_from_files("articles/")
+#SendEmail("smtp.mandrillapp.com",os.environ["SMTP_USER"], os.environ["SMTP_PASSWORD"],  "lars.andersson@lundalogik.se","filip.persson@lundalogik.se", articles)
 
 cpr = bottle.app()
 
 @cpr.route('/')
 def send_static():
-    return static_file('/index.html', root='./')
+    return static_file('/index.html', root='./web')
 
-@cpr.route('/articles')
-def send_static():
-    return articles 
+@cpr.route('/web/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='./web')
 
+@cpr.route('/articles',method='GET')
+def get_articles():
+    return articles
+
+@cpr.route('/send',method='POST')
+def send_articles():
+    print("hepp")
+    return static_file('/index.html', root='./web')
+    #email = request.forms.get('email')
+    # SendEmail("smtp.mandrillapp.com",os.environ["SMTP_USER"], os.environ["SMTP_PASSWORD"],  email ,"info@lundalogik.se", articles)
+    
 
 cpr.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
